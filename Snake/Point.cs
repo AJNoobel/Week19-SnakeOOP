be9c4fd400290b89fed309dcf2508d95d425a207
Opilane TTHK <opilane@tthk.ee>
2022-01-05 16:24:47 +0200

    yes

---
+++ Snake/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char symb;
+
+        public Point(int _x, int _y, char _symb)
+        {
+            x = _x;
+            y = _y;
+            symb = _symb;
+        }
+
+        public void draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(symb);
+        }
+
+
+
+    }
+}
---
+++ Snake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(10, 10, '*');
+            Point p2 = new Point(11, 10, '*');
+
+
+            HorizontalLine hLine = new HorizontalLine(10, 14, 5, '*');
+            hLine.Draw();
+            VerticalLine vLine = new VerticalLine(6, 16, 10, '@');
+            vLine.Draw();
+
+
+            HorizontalLine top = new HorizontalLine(0, 80, 0, '#');
+            top.Draw();
+            Console.ReadLine();
+            VerticalLine left = new VerticalLine(0, 25, 0, '#');
+            left.Draw();
+        }
+
+        
+
+    }
+}
